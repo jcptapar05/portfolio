ca28936f2170fdde981664c3714f3dfe0680f5f2
jcptapar05 <jcptapar05@gmail.com>
2024-11-08 11:30:32 +0800

    Update description

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 
 const page = () => {
- return (
-  <div className="w-screen h-[87vh] flex justify-center items-center">
-   <div className="max-w-[800px] px-6 md:px-0">
-    <h2 className="capitalize text-2xl font-bold">About me</h2>
-    <br /><br />
-    <p className="tracking-wider	">
-     My name is
-     <i> Julius Tapar.</i> I&apos;m from Philippines. Associate Graduated in
-     Computer Science.
-    </p>
-    <br />
-    <p className="mt-2 tracking-wider">
-     I am an experienced frontend developer with expertise in React.js, Next.js,
-     Vue.js, Express.js, MySQL, Prisma, Redux Toolkit, Zustand, Shadcn, and
-     various CSS frameworks.
-    </p>
-    <br />
-    <p className="mt-2 tracking-wider">I hope you enjoy my website.</p>
-   </div>
-  </div>
- );
+  return (
+    <div className="w-screen h-[87vh] flex justify-center items-center">
+      <div className="max-w-[800px] px-6 md:px-0">
+        <h2 className="capitalize text-2xl font-bold">About me</h2>
+        <br />
+        <br />
+        <p className="tracking-wider	">
+          My name is
+          <i> Julius Tapar.</i> I&apos;m from Philippines. Associate Graduated
+          in Computer Science.
+        </p>
+        <br />
+        <p className="mt-2 tracking-wider">
+          I am an experienced frontend developer with knowledge in React.js,
+          Next.js, Vue.js, Express.js, MySQL, Prisma, Redux Toolkit, Zustand,
+          Shadcn, and various CSS frameworks.
+        </p>
+        <br />
+        <p className="mt-2 tracking-wider">I hope you enjoy my website.</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
